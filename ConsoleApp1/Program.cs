37fe9432dbef145fb23c1cf2d5fa9fdc06bc301d
Anthony Nguyen <thang.nguyen1@fractal.vn>
2020-11-18 09:23:42 +0700

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = { 1, 3, 4, 5, 6 };
+            string value = arr.ToString();
+            string[] v1 = value.Split(value, 3);
+            Console.WriteLine(v1);
+        }
+        static string hackerrankInString(string s)
+        {
+            Queue<char> sCheck = new Queue<char>();
+            sCheck.Enqueue('h');
+            sCheck.Enqueue('a');
+            sCheck.Enqueue('c');
+            sCheck.Enqueue('k');
+            sCheck.Enqueue('e');
+            sCheck.Enqueue('r');
+            sCheck.Enqueue('r');
+            sCheck.Enqueue('a');
+            sCheck.Enqueue('n');
+            sCheck.Enqueue('k');
+
+
+            foreach (var x in s)
+            {
+                if (x == sCheck.Peek())
+                {
+                    sCheck.Dequeue();
+                }
+            }
+            if (sCheck != null)
+                return "NO";
+            return "YES";
+        }
+
+        static void kaprekarNumbers(int p, int q)
+        {
+            for (int i = p; i < q; i++)
+            {
+                int count = (i * i).ToString().Length;
+                if (count % 2 != 0 && i != 1)
+                    continue;
+                count /= 2;
+                double v1 = (i * i) / Math.Pow((double)10, (double)count);
+                double v2 = (i * i) % Math.Pow((double)10, (double)count);
+                if ((int)v1 + (int)v2 == i)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+        }
+    }
+}
---
+++ demo/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace demo
+{
+    public class Class1
+    {
+        public static void main(string[] args)
+        {
+            Console.Write("?");
+        }
+    }
+}
